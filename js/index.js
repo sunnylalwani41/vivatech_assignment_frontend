bd0e9991f460c88e10540917596389a54e87609c
sunnylalwani41 <sunnylalwani41@gmail.com>
2026-03-09 16:09:32 +0530

    update the ui

--- js/index.js
+++ js/index.js
@@ -1,12 +1,6 @@
 const BASE_URL = "https://vivatechrndbackend--d9352967050.replit.app";
 
-function addUser(){
-
-    const data = {
-        name: document.getElementById("name").value,
-        contactNumber: document.getElementById("contactNumber").value,
-        roleId: document.getElementById("roleId").value
-    };
+function addUser(data){
 
     fetch(BASE_URL + "/add_user",{
         method:"POST",
@@ -17,50 +11,172 @@ function addUser(){
     })
     .then(res=>res.json())
     .then(data=>{
-        document.getElementById("result").innerText="User Added";
+        closePopup();
+        openOtpVerificationSession();
+
         console.log(data);
     })
     .catch(err=>console.log(err));
 }
 
-
-
 function sendOtp(){
 
-    const mobile=document.getElementById("otpMobile").value;
+    const mobile=document.getElementById("contactNumber").value;
 
     fetch(BASE_URL + "/otp_send?contactNumber="+mobile)
     .then(res=>res.json())
     .then(data=>{
-        document.getElementById("result").innerText="OTP Sent";
+        console.log(data);
     })
     .catch(err=>console.log(err));
 }
 
 
+let userLoginBtn = document.getElementsByClassName("user-login");
 
-function verifyOtp(){
+for(let i=0;i<userLoginBtn.length;i++){
+    userLoginBtn[i].addEventListener("click", () => {
+        document.getElementById("login-background").style.display = "flex";
+    });
+}
 
-    const data={
-        contactNumber:document.getElementById("verifyMobile").value,
-        otp:document.getElementById("otp").value
-    };
+let displayLoginButton = () => {
+    for(let i=0;i<userLoginBtn.length;i++){
+        userLoginBtn[i].style.display = "flex";
+    }
 
-    fetch(BASE_URL + "/otp_submit",{
+    document.querySelector(".user-logout").style.display = "none";
+}
+
+let displayLogoutButton = () => {
+    for(let i=0;i<userLoginBtn.length;i++){
+        userLoginBtn[i].style.display = "none";
+    }
+    document.querySelector(".user-logout").style.display = "flex";
+}
+
+function verifyOtp(contactNumber, otp){
+    // let contactNumber = document.getElementById("verifyMobile").value;
+    // let otp = document.getElementById("otp").value;
+
+    fetch(BASE_URL + "/otp_submit?contactNumber=" + contactNumber + "&otp=" + otp, {
         method:"POST",
         headers:{
             "Content-Type":"application/json"
-        },
-        body:JSON.stringify(data)
+        }
     })
     .then(res=>res.json())
     .then(data=>{
-
         localStorage.setItem("token",data.token);
+        closeOtpPopup();
+        Swal.fire("Login Successfully!");
+        displayLogoutButton();
 
-        document.getElementById("result").innerText="Login Successful";
         console.log(data);
-
     })
     .catch(err=>console.log(err));
 }
+
+function getAllRole(){
+
+    fetch(BASE_URL + "/get_all_role")
+    .then(res=>res.json())
+    .then(data=>{
+        let roleSelect = document.getElementById("roleId");
+        
+        roleSelect.innerHTML = ""; // Clear existing options
+        //default option
+        let defaultOption = document.createElement("option");
+        option.value = "";
+        option.text = "Select Role";
+        roleSelect.appendChild(defaultOption);
+
+        data.forEach(role => {
+            let option = document.createElement("option");
+            option.value = role.id;
+            option.text = role.name;
+            roleSelect.appendChild(option);
+        });
+    })
+    .catch(err=>console.log(err));
+}
+
+let disableLogin = () => {
+    document.getElementById("contactNumber").disabled = true;
+    document.querySelector("#loginForm button[type='submit']").disabled = true;
+}
+
+let closePopup = () =>{
+    document.getElementById("login-background").style.display = "none";
+}
+
+let closeOtpPopup = () =>{
+    document.getElementById("otp-verification-background").style.display = "none";
+}
+
+let openOtpVerificationSession = () => {
+    document.getElementById("otp-verification-background").style.display = "flex";
+
+    document.getElementById("otpForm").addEventListener("submit", (e) => {
+        e.preventDefault();
+        let contactNumber = document.getElementById("contactNumber").value;
+        let otp = document.getElementById("otp").value;
+        verifyOtp(contactNumber, otp);
+    });
+}
+
+let registerationFormDisplay = () => {
+    document.querySelector(".register-container").style.display = "flex";
+
+    document.getElementById("registerForm").addEventListener("click", (e) => {
+        e.preventDefault();
+        
+        const data = {
+            name: document.getElementById("name").value,
+            contactNumber: document.getElementById("contactNumber").value,
+            roleId: document.getElementById("role").value
+        };
+
+        addUser(data);
+    });
+}
+
+function checkUserExistence(){
+
+    const mobile=document.getElementById("contactNumber").value;
+
+    fetch(BASE_URL + "/check_user_exist?contactNumber="+mobile)
+    .then(res=>res.json())
+    .then(data=>{
+        if(data.object === true){
+            disableLogin();
+            sendOtp();
+            closePopup();
+            openOtpVerificationSession();
+        }
+        else{
+            disableLogin();
+            getAllRole();
+            registerationFormDisplay();
+        }
+    })
+    .catch(err=>console.log(err));
+}
+
+document.getElementById("loginForm").addEventListener("submit", (e) => {
+    e.preventDefault();
+    disableLogin();
+    checkUserExistence();
+});
+
+document.querySelector("#resentOtp").addEventListener("click", () => {
+    sendOtp();
+});
+
+document.querySelector(".user-logout").addEventListener("click", () => {
+    localStorage.removeItem("token");
+    Swal.fire("Logout Successfully!");
+    displayLoginButton();
+});
+
+localStorage.getItem("token") ? displayLogoutButton() : displayLoginButton();
